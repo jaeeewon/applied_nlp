59357fe1dae2566e1ce4bc36f07b1e8a3df8d5e7
jaeeewon <hello.pleizz@gmail.com>
2025-12-04 18:42:43 +0900

    chore: decreased per_device_eval_batch_size 64 > 48

--- mini_project/train.py
+++ mini_project/train.py
@@ -204,7 +204,7 @@ if __name__ == "__main__":
         logging_steps=10,
         learning_rate=1e-4,
         per_device_train_batch_size=32,
-        per_device_eval_batch_size=64,
+        per_device_eval_batch_size=48,
         num_train_epochs=30,
         warmup_ratio=0.1,
         weight_decay=0.01,
